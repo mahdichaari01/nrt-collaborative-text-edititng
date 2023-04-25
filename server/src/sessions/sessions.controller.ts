9ce782bf1117eba0b5e6e3d28acc8aa2ae27d397
YassineRaboudi007 <raboudi.yassine007@gmail.com>
2023-04-25 16:20:59 +0100

    service connect user function

--- server/src/sessions/sessions.controller.ts
+++ server/src/sessions/sessions.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Post } from '@nestjs/common';
+import { SessionsService } from './sessions.service';
 
 @Controller('sessions')
-export class SessionsController {}
+export class SessionsController {
+  @Inject(SessionsService)
+  private readonly sessionsService: SessionsService;
+
+  @Post('')
+  async connectToRabbitMq() {
+    return await this.sessionsService.connectUser();
+  }
+}
--- server/src/sessions/sessions.module.ts
+++ server/src/sessions/sessions.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
 import { SessionsController } from './sessions.controller';
+import { RabbitMqManagementModule } from 'src/rabbit-mq-management/rabbit-mq-management.module';
 
 @Module({
   providers: [SessionsService],
-  controllers: [SessionsController]
+  controllers: [SessionsController],
+  imports: [RabbitMqManagementModule],
 })
 export class SessionsModule {}
