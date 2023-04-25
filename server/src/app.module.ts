67557ad0630dd71f911626b74adcc008cdd0d6b5
YassineRaboudi007 <raboudi.yassine007@gmail.com>
2023-04-25 16:26:03 +0100

    some weird error

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitMqManagementModule } from './rabbit-mq-management/rabbit-mq-management.module';
-
 import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
