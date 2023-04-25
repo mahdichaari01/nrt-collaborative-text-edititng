9d4a6b3769076271aea53e9a3429547c86bc38cc
YassineRaboudi007 <raboudi.yassine007@gmail.com>
2023-04-25 15:37:04 +0100

    added sessions module , service and controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitMqManagementModule } from './rabbit-mq-management/rabbit-mq-management.module';
 
+import { SessionsModule } from './sessions/sessions.module';
+
 @Module({
-  imports: [RabbitMqManagementModule],
+  imports: [SessionsModule, RabbitMqManagementModule],
   controllers: [AppController],
   providers: [AppService],
 })
